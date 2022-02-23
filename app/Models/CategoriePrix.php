6ce08d734d8fd13cde87f024e7ca51176123f2a9
editho512 <alex.editho512@gmail.com>
2022-02-23 09:58:06 +0300

    ajout des migration pour generer les table de la base de donnée

---
+++ app/Models/CategoriePrix.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CategoriePrix extends Model
+{
+    use HasFactory;
+}
